08f9adf1fcc0b6eecae553e49adb09417de9fd1b
Samer Aldefai <samer.aldefai@sap.com>
2019-12-25 02:01:17 +0100

    Revert "Revert "java parser running""
    This reverts commit cd23dfb8148a6a94c4fa376217c74365e1f9aa3a.

--- src/extensions.ts
+++ src/extensions.ts
@@ -6,13 +6,14 @@ interface Array<T> {
 Array.prototype.last = function<T>() : T {
     return this[this.length-1];
 }
-
+/*
 Array.prototype.groupBy =  function<T>(key:string) : T[][] {
     let me: T[] = this;
-    let g = me.reduce( (group, current, []) => {
+    let g = me.reduce( (group, current, T[]) => {
         group[current[key]] = group[current[key]] || [];
         group[current[key]].push(current);
         return group
     }, [] );
     return g;
 }
+*/
